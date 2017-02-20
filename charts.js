de990c9557a3bdbb49419cb8ecbd57e07b59faf3
Ziyu Zhang <zhangziyuchina@gmail.com>
2017-02-20 16:26:33 -0600

    modified Spending and Home

--- charts.js
+++ charts.js
@@ -1,28 +1,37 @@
-google.charts.load("current", {packages:["corechart", 'table']});
+google.charts.load("current", {packages:["corechart", 'table', 'bar']});
 google.charts.setOnLoadCallback(drawChart);
 function drawChart() {
 	var data = google.visualization.arrayToDataTable([
-		['Location', 'Number of Visits'],
-		['at home',  							12],
+		['Location', 							'Visits'],
+		['at home',  							14],
 		['527 Caffe',							5],
 		['Subway @ Norris University Center', 	4],
 		["Giordano's", 							3],
 		["LYFE Kitchen", 						8]]);
 	var options = {
-		title: 'Dining Locations - Total: 32 Visits',
-		pieHole: 0.4,
-		fontSize: 12
+		title: 'Dining Locations - Total: 34 Visits',
+		fontSize: 12,
+		chartArea: {width: '50%'},
+		colors: ['green']
+		// hAxis: {
+		// 	title: 'No. of Visits',
+		// 	minValue: 0
+		// },
+		// vAxis: {
+		// 	title: 'Location'
+		// }
 		// width: '100%',
 		// height: '100%'
+
 	};
-	var chart = new google.visualization.PieChart(document.getElementById('donut_chart'));
+	var chart = new google.visualization.BarChart(document.getElementById('donut_chart'));
 	chart.draw(data, options);
 }
 
 google.charts.setOnLoadCallback(drawChart2);
 function drawChart2() {
 	var data = google.visualization.arrayToDataTable([
-		['Expenditure', 'Dollars'],
+		['Expenditure', 						'U.S. Dollars'],
 		['at home',  							178.88],
 		['527 Caffe',							68.12],
 		['Subway @ Norris University Center', 	120.48],
@@ -30,12 +39,20 @@ function drawChart2() {
 		["LYFE Kitchen", 						116.72]]);
 	var options = {
 		title: 'Expenditures - Total: 540.63 U.S. Dollars',
-		pieHole: 0.4,
-		fontSize: 12
+		fontSize: 12,
+		chartArea: {width: '50%'},
+		colors: ['purple']
+		// hAxis: {
+		// 	title: 'No. of Visits',
+		// 	minValue: 0
+		// },
+		// vAxis: {
+		// 	title: 'Location'
+		// }
 		// width: '100%',
 		// height: '50%'
 	};
-	var chart = new google.visualization.PieChart(document.getElementById('donut_chart_2'));
+	var chart = new google.visualization.BarChart(document.getElementById('donut_chart_2'));
 	chart.draw(data, options);
 }
 
@@ -49,8 +66,8 @@ function drawChart3() {
 		['2/17/2017',  26.47]]);
 
 	var options = {
-		title: 'Expenditure by Day (in U.S. Dollars)',
-		curveType: 'function',
+		title: 'Expenditures by Day (in U.S. Dollars)',
+		// curveType: 'function',
 		legend: {position: 'none'},
 		fontSize: 12
 		// width: '100%',
@@ -76,7 +93,7 @@ function drawTable() {
 	var options = {
 		showRowNumber: true,
 		width: '100%',
-		height: '100%'
+		height: '100%',
 	}
 	var table = new google.visualization.Table(document.getElementById('table_chart'));
 	table.draw(data, options);
